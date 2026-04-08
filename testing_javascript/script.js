0b6aa363b600217dd6befb983bfdd6b4602dfdc4
nick796 <32782139+nick796@users.noreply.github.com>
2026-04-08 21:51:37 +0300

    Add files via upload

--- testing_javascript/script.js
+++ testing_javascript/script.js
@@ -4,6 +4,22 @@ const button = document.getElementById('btn-palette');
 // 2. Βρίσκουμε το μέρος που θα μπούν τα χρώματα
 const grid = document.getElementById('colors-grid');
 
+
+
+function hslToHex(h, s, l) {
+    l /= 100;
+    const a = (s * Math.min(l, 1 - l)) / 100;
+    const f = (n) => {
+        const k = (n + h / 30) % 12;
+        const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+        return Math.round(255 * color).toString(16).padStart(2, '0');
+    };
+    return `#${f(0)}${f(8)}${f(4)}`.toUpperCase();
+}
+
+
+
+
 // 3. Λέμε στον "υπάλληλο" τι να κάνει όταν κάποιος πατήσει το κουμπί
 button.onclick = function() {
     
@@ -26,17 +42,39 @@ button.onclick = function() {
 
         // Δημιουργούμε ένα νέο "κουτάκι" (div) στη μνήμη
         let card = document.createElement("div");
+// --------
+        let hexCode = hslToHex(hue,70,60)
         
         // Του δίνουμε την κλάση CSS που φτιάξαμε για να φαίνεται όμορφο
         card.className = "color-card";
 
         // Βάζουμε μέσα στο κουτάκι το χρώμα και το όνομά του
         card.innerHTML = `
-            <div class="color-box" style="background-color: ${colorName}"></div>
-            <div class="color-info">${colorName}</div>
+            <div class="color-box" style="background-color: ${hexCode}"></div>
+            <div class="color-info">${hexCode}</div>
            
         `;
 
+
+
+        // --- ΤΟ ΚΟΜΜΑΤΙ ΤΗΣ ΑΝΤΙΓΡΑΦΗΣ ---
+    card.onclick = function() {
+        // Αντιγράφουμε το κείμενο στο πρόχειρο
+        navigator.clipboard.writeText(hexCode);
+        
+        // Μικρό εφέ: Αλλάζουμε το κείμενο προσωρινά
+        let infoBox = card.querySelector('.color-info');
+        let originalText = infoBox.innerText;
+        
+        infoBox.innerText = "Copied!";
+        infoBox.classList.add('copied-anim'); // Προσθέτουμε το animation
+        
+        // Μετά από 1 δευτερόλεπτο, επανέρχεται στο κανονικό
+        setTimeout(() => {
+            infoBox.innerText = originalText;
+            infoBox.classList.remove('copied-anim');
+        }, 1000);
+    };
         // Τέλος, το εμφανίζουμε στη σελίδα μας
         grid.appendChild(card);
     }
